9d0726e0fb7a212ce3e4da2fe457ddbf6e92dead
Yogesh Samtani <yogeshsamtani@gmail.com>
2024-08-19 14:15:42 +0200

    añadir click en cartas

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,17 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import Mantenimiento from "../components/Mantenimiento";
 import Parking from "../components/Parking";
 import Tarjetas from "../components/Tarjetas";
 
 function Home({ onClose, openModal }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between gap-[15px]  m-10   h-[70vh]">
-      <div className="bg-custom-gradient2 rounded-29px shadow-custom2 p-6  m-15   min-h-[500px]  hover:bg-custom-gradient3">
+      <div
+        className="bg-custom-gradient2 rounded-29px shadow-custom2 p-6  m-15   min-h-[500px]  hover:bg-custom-gradient3"
+        onClick={() => navigate("/tarjetas")}
+      >
         <Tarjetas />
       </div>
-      <div className="bg-custom-gradient2 rounded-29px shadow-custom2  p-6 m-15   min-h-[500px] hover:bg-custom-gradient3">
+      <div
+        className="bg-custom-gradient2 rounded-29px shadow-custom2  p-6 m-15   min-h-[500px] hover:bg-custom-gradient3"
+        onClick={() => navigate("/parking")}
+      >
         <Parking />
       </div>
-      <div className="bg-custom-gradient2 rounded-29px shadow-custom2 p-6 m-15  min-h-[500px]  hover:bg-custom-gradient3">
+      <div
+        className="bg-custom-gradient2 rounded-29px shadow-custom2 p-6 m-15  min-h-[500px]  hover:bg-custom-gradient3"
+        onClick={() => navigate("/mantenimiento")}
+      >
         <Mantenimiento onClose={onClose} openModal={openModal} />
       </div>
     </div>
